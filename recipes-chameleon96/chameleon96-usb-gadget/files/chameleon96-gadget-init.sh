8da4b451f373929e4b2ccf42b3fc91121bd16ea5
Dan Negvesky <dnegvesky@arrow.com>
2017-02-02 17:30:25 -0500

    removed to try to fix bitbake error

--- recipes-chameleon96/chameleon96-usb-gadget/files/chameleon96-gadget-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-modprobe g_multi file=/usr/share/chameleon96-usb-gadget/fat_files.img
-
-sleep 5
-
-/sbin/ifconfig usb0 hw ether 00:01:02:03:c5:96
-/sbin/ifconfig usb0 192.168.96.96 netmask 255.255.255.0
-/usr/sbin/udhcpd -fS -I 192.168.96.96 /etc/udhcpd.conf
